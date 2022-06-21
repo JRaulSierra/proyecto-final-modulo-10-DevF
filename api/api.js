575a899fbc80d7ca054e5d8bacc54f3c7861c1f7
Raul Sierra <j.raulsierra.uni@gmail.com>
2022-06-20 19:00:07 -0600

    feat: configuracion de server y environment

---
+++ api/api.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import config from './config/index.js';
+import morgan from 'morgan';
+
+const api = express();
+
+api.use(morgan('tiny'));
+api.use(express.json());
+
+api.get('/status', (req, res) => {
+    return res.json({
+        msg: 'Api funcionando',
+    });
+});
+
+api.use((err, req, res, next) => {
+    if (
+      config.server.enviroment === 'development' ||
+      config.server.enviroment === 'local' ||
+      config.server.enviroment === 'test'
+    ) {
+      return res.status(500).json({
+        msg: 'Error',
+        error: err,
+      });
+    }
+    return res.status(500).json({
+      msg: 'Ha ocurrido un error en el server, comunícate con un desarrollador',
+    });
+  });
+  
+  export default api;
+  
---
+++ api/config/index.js
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default {
+    server: {
+        port : process.env.PORT || 3000,
+        environment: process.env.NODE_ENV || 'local'
+    }
+}
